33207d46d0bbe9b8d189af41ca76067c6cc8d168
Michael Kukarkin <kukarkinmm@gmail.com>
2019-01-09 03:46:15 +0300

    message_test

---
+++ src/test/message/message_test.cpp
@@ -0,0 +1,29 @@
+//
+// Created by michael on 08.01.19.
+//
+
+#include <gtest/gtest.h>
+#include <tasks/message/message.h>
+#include <string>
+#include <sstream>
+
+TEST(MessageTest, format) {
+std::stringstream test_str;
+message(test_str, "% + % = %\n", 'a', 2, 3.0);
+
+EXPECT_TRUE(std::string("a + 2 = 3\n") == test_str.str());
+}
+
+TEST(MessageTest, argsOne) {
+std::stringstream test_str;
+message(test_str, "% + % = %\n", 'a', 2, 3.0, "oneMore");
+
+EXPECT_TRUE(std::string("a + 2 = 3\n") == test_str.str());
+}
+
+TEST(MessageTest, argsTwo) {
+std::stringstream test_str;
+message(test_str, "% + % = %\n", 'a', 2);
+
+EXPECT_TRUE(std::string("a + 2 = %\n") == test_str.str());
+}
