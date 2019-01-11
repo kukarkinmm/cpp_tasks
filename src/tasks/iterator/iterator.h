f4de4407913f3812a15607a09eea2ada58ff2ecd
Michael Kukarkin <kukarkinmm@gmail.com>
2019-01-11 05:08:25 +0300

    iterator fix

--- src/tasks/iterator/iterator.h
+++ src/tasks/iterator/iterator.h
@@ -13,14 +13,14 @@ class iterator : public std::iterator<std::output_iterator_tag, void, void, void
     const CharT* delim;
 
 public:
-    typedef CharT char_type;
-    typedef Traits traits_type;
-    typedef std::basic_ostream<CharT, Traits> ostream_type;
+    using char_type = CharT;
+    using traits_type = Traits;
+    using ostream_type = std::basic_ostream<CharT, Traits>;
 
     explicit iterator(ostream_type& s) : out_stream(&s), delim(0) {}
     iterator(ostream_type& s, const CharT* delimiter) : out_stream(&s), delim(delimiter) {}
     iterator(const iterator<T, CharT, Traits>& x) : out_stream(x.out_stream), delim(x.delim) {}
-    ~iterator() {}
+    ~iterator() = default;
 
     iterator<T, CharT, Traits>& operator=(const T& value) {
         *out_stream << value;
@@ -28,9 +28,10 @@ public:
             *out_stream << delim;
         return *this;
     }
+    
     iterator<T, CharT, Traits>& operator*() { return *this; }
     iterator<T, CharT, Traits>& operator++() { return *this; }
-    iterator<T, CharT, Traits>& operator++(int) { return *this; }
+    const iterator<T, CharT, Traits> operator++(int) { return *this; }
 };
 
 #endif //CP_TASKS_ITERATOR_H
